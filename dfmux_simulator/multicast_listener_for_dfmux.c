ecfef8e3d513b84a0d38c7003af9ab4cbf40f1df
ckjellstrand <bkjell@gmail.com>
2015-06-30 11:27:24 -0400

    Used more sophisticated casting that a) works and b) is faster. Both algorithms are present in struct_casting.c and multicast_listener_for_dfmux.c now uses the better one. Also added writing to file.

--- dfmux_simulator/multicast_listener_for_dfmux.c
+++ dfmux_simulator/multicast_listener_for_dfmux.c
@@ -9,12 +9,37 @@
 #include <stdlib.h>
 
 
-//#define HELLO_PORT 12345
-//#define HELLO_GROUP "225.0.0.37"
-//#define MSGBUFSIZE 256
 #define HELLO_PORT 9876
 #define HELLO_GROUP "239.192.0.2" 
 #define MSGBUFSIZE 560
+
+
+typedef struct Timestamp{
+	/* SIGNED types are used here to permit negative numbers during
+	 *          * renormalization and comparison. THE ORDER IS IMPORTANT, since
+	 *                   * this matches the VHDL's field packing. */
+	int32_t y,d,h,m,s;
+	int32_t ss;
+	int32_t c, sbs;
+} Timestamp;
+
+
+typedef struct DfmuxPacket{
+	uint32_t magic;
+	uint32_t version;
+
+	uint8_t num_modules;
+	uint8_t channels_per_module;
+	uint8_t fir_stage;
+	uint8_t module; /* linear; 0-7. don't like it much. */
+
+	uint32_t seq; /* incrementing sequence number */
+
+	int32_t s[128];
+
+	struct Timestamp ts;
+} DfmuxPacket;
+
 
 main(int argc, char *argv[])
 {
@@ -22,9 +47,7 @@ main(int argc, char *argv[])
 	int fd, nbytes,addrlen;
 	struct ip_mreq mreq;
 	char msgbuf[MSGBUFSIZE];
-	char msgbuftest[MSGBUFSIZE];
 
-	u_int yes=1;            /*** MODIFICATION TO ORIGINAL */
 
 	/* create what looks like an ordinary UDP socket */
 	fd=socket(AF_INET,SOCK_DGRAM,0);
@@ -34,13 +57,13 @@ main(int argc, char *argv[])
 	}
 
 
-	/**** MODIFICATION TO ORIGINAL */
 	/* allow multiple sockets to use the same PORT number */
+	u_int yes=1;
 	if (setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(yes)) < 0) {
 		perror("Reusing ADDR failed");
 		exit(1);
 	}
-	/*** END OF MODIFICATION TO ORIGINAL */
+
 
 	/* set up destination address */
 	memset(&addr,0,sizeof(addr));
@@ -62,7 +85,12 @@ main(int argc, char *argv[])
 		exit(1);
 	}
 
-	/* now just enter a read-print loop */
+
+	/* Set up output file. */
+	FILE *fileout;
+	fileout = fopen("/home/bjorn/ebex_data_handler/dfmux_simulator/parsed_data/out.txt", "w+");
+	int i;
+
 	while (1) {
 		addrlen=sizeof(addr);
 
@@ -71,9 +99,21 @@ main(int argc, char *argv[])
 			perror("recvfrom");
 			exit(1);
 		}
-		//strncpy(msgbuftest, msgbuf, 9);
-		//puts(msgbuftest);
-		puts(msgbuf);
+		//puts(msgbuf);
+		DfmuxPacket *tmp = (DfmuxPacket*) msgbuf;
+		printf("%u %u %u\n", tmp->seq, tmp->fir_stage, tmp->module);
+		for (i = 0; i<128; i++){
+			printf("%d ", tmp->s[i]);
+		}
+		printf("\n");
+
+		fprintf(fileout, "%u %u %u\n", tmp->seq, tmp->fir_stage, tmp->module);
+		for (i = 0; i<128; i++){
+			fprintf(fileout, "%d ", tmp->s[i]);
+		}
+		fprintf(fileout, "\n");
+
+
 	}
 }
 
--- testing_and_examples/struct_casting.c
+++ testing_and_examples/struct_casting.c
@@ -3,12 +3,39 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 
-typedef struct AMG_ANGLES {
+typedef struct {
 	float yaw;
 	float pitch;
 	float roll;
 } AMG_ANGLES;
+
+typedef struct Timestamp{
+	/* SIGNED types are used here to permit negative numbers during
+	 * renormalization and comparison. THE ORDER IS IMPORTANT, since
+	 * this matches the VHDL's field packing. */
+	int32_t y,d,h,m,s;
+	int32_t ss;
+	int32_t c, sbs;
+} Timestamp;
+
+typedef struct DfmuxPacket{
+	uint32_t magic;
+	uint32_t version;
+
+	uint8_t num_modules;
+	uint8_t channels_per_module;
+	uint8_t fir_stage;
+	uint8_t module; /* linear; 0-7. don't like it much. */
+
+	uint32_t seq; /* incrementing sequence number */
+
+	int32_t s[128];
+
+	struct Timestamp ts;
+} DfmuxPacket;
+
 
 int main() {
 	AMG_ANGLES struct_data;
@@ -24,4 +51,18 @@ int main() {
 	AMG_ANGLES tmp; //Re-make the struct
 	memcpy(&tmp, b, sizeof(tmp));
 	printf("%f\n", tmp.yaw);
+
+	AMG_ANGLES *tmp2 = (AMG_ANGLES*) b;
+	printf("%f\n", tmp2->yaw);
+
+	DfmuxPacket packet;
+	packet.magic = 1;
+	printf("%u\n", packet.magic);
+	char c[sizeof(packet)];
+	memcpy(c, &packet, sizeof(packet));
+	DfmuxPacket *tmp3 = (DfmuxPacket*) c;
+	printf("%u\n", tmp3->magic);
+
+
+
 }
